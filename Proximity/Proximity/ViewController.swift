04992cca875fec0710ff30c8d86e1f670f1a2897
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2017-10-07 17:54:33 -0400

    Add XCode project

---
+++ Proximity/Proximity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proximity
+//
+//  Created by Christopher Perkins on 10/7/17.
+//  Copyright © 2017 Christopher Perkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
