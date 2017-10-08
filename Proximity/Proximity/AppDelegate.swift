d4490ca792c163e669cba492be952dfe6e2a8f7e
Grant Mullinax <grant.mullinax@siemens.com>
2017-10-08 04:55:49 -0400

    A piece of google's maps trash.

--- Proximity/Proximity/AppDelegate.swift
+++ Proximity/Proximity/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        GMSServices.provideAPIKey("AIzaSyAzaPsFE6gU4pMwMyVic6QkzvwczETGobg")
+        GMSPlacesClient.provideAPIKey("AIzaSyAzaPsFE6gU4pMwMyVic6QkzvwczETGobg")
         // Override point for customization after application launch.
         return true
     }
--- Proximity/Proximity/AreaViewController.swift
+++ Proximity/Proximity/AreaViewController.swift
@@ -6,8 +6,54 @@
 //  Copyright © 2017 Christopher Perkins. All rights reserved.
 //
 
-import UIKit
+import CoreLocation
+import GoogleMaps
+import GooglePlaces
 
-class AreaViewController: UIViewController {
-    // grant you're dumb
+class AreaViewController: UIViewController, CLLocationManagerDelegate {
+    let size:Int32 = 10
+    
+    
+    //Map
+    
+    
+    
+    var map: GMSMapView?
+    
+    let tileLayer = GMSURLTileLayer()
+    let manager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 0, longitude: 0, zoom: 10)
+        map = GMSMapView.map(withFrame: .zero, camera: camera)
+        view = map
+        
+        tileLayer.map = map
+        tileLayer.zIndex = size
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        
+        
+        
+        let location = locations[0]
+        
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        //let region:GMSCoordinateBounds = GMSCoordinateBounds(coordinate: myLocation, coordinate: span)
+        
+        //map.setRegion(region, animated: true)
+        //self.map.showsUserLocation = true
+    }
+    
+    @IBOutlet weak var locationGrid: UICollectionView!
 }
