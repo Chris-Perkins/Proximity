e9e419f09ea6f85869a6fb5aca6985a6673579bc
Grant Mullinax <grant.mullinax@siemens.com>
2017-10-08 05:01:57 -0400

    Apple makes works.

--- Proximity/Proximity/AreaViewController.swift
+++ Proximity/Proximity/AreaViewController.swift
@@ -7,53 +7,34 @@
 //
 
 import CoreLocation
-import GoogleMaps
-import GooglePlaces
+import UIKit
+import MapKit
 
 class AreaViewController: UIViewController, CLLocationManagerDelegate {
-    let size:Int32 = 10
-    
-    
     //Map
-    
-    
-    
-    var map: GMSMapView?
-    
-    let tileLayer = GMSURLTileLayer()
+    @IBOutlet weak var map: MKMapView!
+
     let manager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let camera = GMSCameraPosition.camera(withLatitude: 0, longitude: 0, zoom: 10)
-        map = GMSMapView.map(withFrame: .zero, camera: camera)
-        view = map
-        
-        tileLayer.map = map
-        tileLayer.zIndex = size
+
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
-        
     }
-    
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
-        
-        
-        
         let location = locations[0]
         
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
         let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
         
-        //let region:GMSCoordinateBounds = GMSCoordinateBounds(coordinate: myLocation, coordinate: span)
-        
-        //map.setRegion(region, animated: true)
-        //self.map.showsUserLocation = true
+        map.setRegion(region, animated: true)
+        self.map.showsUserLocation = true
     }
-    
-    @IBOutlet weak var locationGrid: UICollectionView!
+
 }
